95eb0a8c7c120b8ff48025ab5067bb020ad4c16d
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2015-09-26 23:19:17 -0300

    organizando classes de routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -8,16 +8,15 @@ class Auth {
   }
 
   getToken(done) {
-    request({
+    let opts = {
       method: "POST",
       url: `${NTask.URL}/token`,
       body: {
         email: this.email,
         password: this.password
       }
-    }, (err, resp, body) => {
-      done(body);
-    });
+    };
+    request(opts, (err, resp, body) => done(body));
   }
 }
 module.exports = Auth;
